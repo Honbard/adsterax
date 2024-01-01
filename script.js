10d5d64b46c5d47cd1893fef0a8fcfa8ea333c22
Honbard <145313410+Honbard@users.noreply.github.com>
2024-01-01 14:36:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12") {
+    e.preventDefault();
+  }
+});
+
+<script type="text/javascript">
+	atOptions = {
+		'key' : '11302f3618d256838185ccee5b3475e8',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/11302f3618d256838185ccee5b3475e8/invoke.js"></scr' + 'ipt>');
+</script>
